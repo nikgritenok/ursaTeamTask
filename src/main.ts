94f32dca55bb70793d663b6734341a756c729d38
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2025-10-31 19:55:22 +0700

    [MASTER][refactor, build] - Удалены компоненты, стили и ассеты из шаблона проекта vue - Удален pinia store из шаблона - Добавлен vuetify - Добавлен плагин vite-vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 import App from './App.vue'
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'light',
+  },
+})
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia()).use(vuetify)
 
 app.mount('#app')
